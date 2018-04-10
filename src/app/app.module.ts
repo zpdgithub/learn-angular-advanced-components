ed10704e87645526830de8cdbf571b3637aa048e
peter <peter@soez.io>
2018-04-10 21:23:27 +0800

    添加内部样式组件InlineStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 
+// 内部样式
+@Component({
+  selector: 'inline-style',
+  styles: [`
+  .highlight {
+    border: 2px solid red;
+    background-color: yellow;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  `],
+  template: `
+  <h4 class="ui horizontal divider header">
+    Inline style example
+  </h4>
+
+  <div class="highlight">
+    This uses component <code>styles</code>
+    property
+  </div>
+  `
+})
+class InlineStyle {
+}
+
+
 @Component({
   selector: 'app-root',
-  template: `(root)`
+  template: `
+  <inline-style></inline-style>
+  `
 })
 export class AppComponent {
 }
@@ -12,7 +40,8 @@ export class AppComponent {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InlineStyle
   ],
   imports: [
     BrowserModule
