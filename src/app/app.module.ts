1b18000727b3e50cb417c3f9a57227ebe61badcc
peter <peter@soez.io>
2018-04-10 21:52:13 +0800

    添加ViewEncapsulation.None组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,7 +45,7 @@ class InlineStyle {
 class ExternalStyle {
 }
 
-// native
+// Shadow DOM组件，将元素进行封装
 @Component({
   selector: `native-encapsulation`,
   styles: [`
@@ -69,12 +69,37 @@ class ExternalStyle {
 class NativeEncapsulation {
 }
 
+// ViewEncapsulation.None组件，不进行封装
+@Component({
+  selector: `no-encapsulation`,
+  styles: [`
+  .highlight {
+    border: 2px dashed red;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  `],
+  template: `
+  <h4 class="ui horizontal divider header">
+    No encapsulation example
+  </h4>
+
+  <div class="highlight">
+    This component uses <code>ViewEncapsulation.None</code>
+  </div>
+  `,
+  encapsulation: ViewEncapsulation.None
+})
+class NoEncapsulation {
+}
+
 @Component({
   selector: 'app-root',
   template: `
   <inline-style></inline-style>
   <external-style></external-style>
   <native-encapsulation></native-encapsulation>
+  <no-encapsulation></no-encapsulation>
   `
 })
 export class AppComponent {
@@ -86,7 +111,8 @@ export class AppComponent {
     AppComponent,
     InlineStyle,
     ExternalStyle,
-    NativeEncapsulation
+    NativeEncapsulation,
+    NoEncapsulation
   ],
   imports: [
     BrowserModule
