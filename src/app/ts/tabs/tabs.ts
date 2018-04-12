842eca7e5a6d6f94cd6e65df40901497852ab695
peter <peter@soez.io>
2018-04-12 10:29:15 +0800

    添加Tabset组件

--- src/app/ts/tabs/tabs.ts
+++ src/app/ts/tabs/tabs.ts
@@ -1,7 +1,10 @@
 import {
   NgModule,
   Component,
-  Input
+  QueryList,
+  AfterContentInit,
+  Input,
+  ContentChildren
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -20,4 +23,36 @@ class Tab {
   @Input() title: string;
   active: boolean = false;
   name: string;
+}
+
+@Component({
+  selector: 'tabset',
+  template: `
+  <div class="ui top attached tabular menu">
+    <a *ngFor="let tab of tabs"
+       class="item"
+       [class.active]="tab.active"
+       (click)="setActive(tab)">
+
+      {{ tab.title }}
+
+    </a>
+  </div>
+  <ng-content></ng-content>
+  `
+})
+class Tabset implements AfterContentInit {  // 一旦子组件的内容初始化，就调用类的方法（ngAfterContentInit）
+  @ContentChildren(Tab) tabs: QueryList<Tab>; // 同时使用QueryList和ContentChildren，将匹配查询的组件填充到QueryList，在应用状态变更时保持这些填充项的更新
+
+  constructor() {
+  }
+
+  ngAfterContentInit() {
+    this.tabs.toArray()[0].active = true;
+  }
+
+  setActive(tab: Tab) {
+    this.tabs.toArray().forEach((t) => t.active = false);
+    tab.active = true;
+  }
 }
